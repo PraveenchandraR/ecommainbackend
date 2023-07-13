1c6f6b59fdbfef2b16ac62106a283e3e7ff2740b
Praveen <praveenchandraravuri@gmail.com>
2023-07-13 14:43:27 +0530

    ecommainbackend

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ dotenv.config({path:"./config.env"})
 const PORT = process.env.PORT || 8000;
 
 // const DB = process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
-const DB = 'mongodb://127.0.0.1:27017/E-commers';
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+// const DB = 'mongodb://127.0.0.1:27017/E-commers';
 
 mongoose.connect(DB,{
     useNewUrlParser:true
